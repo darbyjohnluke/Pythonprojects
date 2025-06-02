c4caef93ad52ceb33731aac5838522e107f3dfd0
darbyjohnluke <138208305+darbyjohnluke@users.noreply.github.com>
2025-06-02 12:14:22 +0530

    Update Hangman.py
    Added Hangman images, added stagecounter, reduced the count to 6 in while loop

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,66 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+stagecounter = 6
 word_list = ["aardvark", "baboon", "camel"]
 randomword = random.choice(word_list)
 gameover = 0
 guessedletters = []
-while gameover < 7:
+while gameover < 6:
         guess = input("Guess a letter!\n")
         showword = ""
         if guess in randomword:
@@ -17,12 +74,16 @@ while gameover < 7:
                 else:
                     showword += "_"
             if "_" not in showword:
-                 gameover = 7
+                 gameover = 6
                  print("You've won")
             print(showword)
+            print(stages[stagecounter])
 
         else:
             print("incorrect")
             gameover += 1
+            stagecounter -= 1
+            print(stages[stagecounter])
+
 print("gameover")
 print(showword)
